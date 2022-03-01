e572cfbcf955485d94c2398b17e324df9467c5f4
unknown <shady.samara@gmail.com>
2022-02-28 23:17:23 -0800

    flutter 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iug_1/enums.dart';
+import 'package:flutter_iug_1/student.dart';
 
 void main() {
-  runApp(MaterialApp(home: IugUi()));
+  runApp(MaterialApp(home: FirstScreen(),));
 }
-class IugUi extends StatelessWidget{
+class FirstScreen extends StatelessWidget{
+  Student student = Student('omar', 'gaza', Gender.male);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      body: Center(child: Text('hello world'),),
-    );
+   return Scaffold(
+     appBar:AppBar(
+       centerTitle: true,
+       backgroundColor:Colors.blue,
+       title: Text('hello'),
+     ) ,
+     body: Container(
+       color: Colors.red,
+       height: 150,
+       child: Column(
+         mainAxisAlignment:MainAxisAlignment.start ,
+         mainAxisSize:MainAxisSize.min ,
+         children: [
+         Text('First Widget InColumn'),
+         Text('Second Widget InColumn'),
+         Text('Third Widget InColumn'),
+         Text('Fourth Widget InColumn'),
+          Text('Fifth Widget InColumn')
+       ],),
+     ),
+    //  Center(child: Text('scaffold body')),
+     bottomNavigationBar: BottomNavigationBar(items: [
+    BottomNavigationBarItem(icon: Text('Home'),label: 'Home'),
+     BottomNavigationBarItem(icon: Icon(Icons.favorite),label: 'favourite'),
+     BottomNavigationBarItem(icon: Icon(Icons.supervised_user_circle),label: 'Profile'),
+     ],)
+   );
   }
-
 }
-// git add .
-// git commit -m 'any commit'
-// git push
---
+++ lib/student.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter_iug_1/enums.dart';
+
+class Student{
+  String name;
+  String schoolName;
+  Gender gender;
+  Student(this.name,this.schoolName,this.gender);
+}
